818763fa65b21d95ea6071853d5a0fa348b2b14a
Christian Angermann <christian.angermann@visable.com>
2022-06-03 11:03:02 +0200

    chore: align aliases

--- oh-my-zsh/custom/internal.aliases.zsh
+++ oh-my-zsh/custom/internal.aliases.zsh
@@ -10,7 +10,6 @@ alias gm="git rebase "
 alias yt="yarn test"
 alias ytw="yarn test:watch"
 alias yb="yarn build"
-alias yl="yarn lint"
 alias ybd="yarn build:dev"
 
 # wlw/visbale docker
@@ -26,18 +25,15 @@ alias k='clear'
 # bundle
 alias be='bundle exec'
 alias bi='bundle install'
-alias rwst="bundle exec rake web_service:test:all"
 alias server="sudo python -m SimpleHTTPServer 80"
 
-alias accounts='cd ~/workspace/wlw/accounts'
-alias cr='cd ~/workspace/wlw/customer_report'
+alias cr='cd ~/workspace/wlw/customer-report-frontend'
 alias lyc='cd ~/workspace/wlw/lyc'
-alias orders='cd ~/workspace/wlw/orders'
-alias products='cd ~/workspace/wlw/products'
+alias sf='cd ~/workspace/wlw/supplier-frontend'
+alias sfe='cd ~/workspace/wlw/supplier-facts-frontend'
 alias sg='cd ~/workspace/wlw/wlw_styleguide'
 alias statistics='cd ~/workspace/wlw/statistics'
 alias user='cd ~/workspace/wlw/user'
-alias wlw='cd ~/workspace/wlw/wlw'
 alias work='cd ~/workspace'
 
 # rebase from either master or main branch required
