50d588d54786bc6ace841095a4bcbcbd13bc201e
Christian Angermann <christian.angermann@visable.com>
2022-08-24 22:49:28 +0200

    feat(neovim): improve bufferline tab styling

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -1,13 +1,7 @@
-local loaded, bufferline = pcall(require, 'bufferline')
-if (not loaded) then return end
+local ok, bufferline = pcall(require, "bufferline")
+local theme_ok, _ = pcall(require, 'nightfox')
+if not ok or not theme_ok then return end
 
-vim.opt.termguicolors = true
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
-
-local loaded, nightfox = pcall(require, 'nightfox')
-
-if (not loaded) then return end
 
 local color = require('nightfox.palette').load('terafox')
 -- print(vim.inspect(color))
@@ -15,41 +9,35 @@ local color = require('nightfox.palette').load('terafox')
 bufferline.setup({
   options = {
     mode = 'tabs',
-    separator_style = 'slant',
+    separator_style = 'thick',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true,
-    diagnostics = 'nvim_lsp',
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match('error') and " " or " "
-      return " " .. icon .. count
-    end
+    color_icons = true
   },
-
   highlights = {
     fill = {
-      fg = color.bg0,
-      -- bg = color.bg0,
+      fg = color.magenta.base,
+      bg = color.green.base,
+    },
+    separator = {
+      fg = '#073642',
+      bg = '#002b36',
+    },
+    separator_selected = {
+      fg = '#073642',
     },
     background = {
-      --   fg = color.red.base,
-      bg = color.bg0,
-    },
-    tab = {
-      fg = color.bg0,
-    },
-    warning_selected = {
-      bold = true,
-      italic = false,
-    },
-    error_selected = {
-      bold = true,
-      italic = false,
+      fg = '#657b83',
+      bg = '#002b36'
     },
     buffer_selected = {
-      bold = true,
-      italic = false,
+      fg = color.fg2,
+      bold = false,
+      italic = false
     },
   },
 })
+
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
