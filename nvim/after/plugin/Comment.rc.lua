ecd1b3277fbc37e72ec4cf09ed9b8e8b21df9541
Christian Angermann <christian.angermann@visable.com>
2022-08-25 22:54:13 +0200

    feat(neovim): set up options

--- nvim/after/plugin/Comment.rc.lua
+++ nvim/after/plugin/Comment.rc.lua
@@ -8,15 +8,15 @@ comment.setup({
   ---@type table
   toggler = {
     ---Line-comment toggle keymap
-    line = '<leader>c<Space>',
+    line = '<leader>c',
     ---Block-comment toggle keymap
-    block = '<leader>b<Space>',
+    block = '<leader>b',
   },
   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
   opleader = {
     ---Line-comment keymap
-    line = '<leader>c',
+    line = '<leader>c<Space>',
     ---Block-comment keymap
-    block = '<leader>b',
+    block = '<leader>b<Space>',
   },
 })
--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -4,31 +4,26 @@ if (not loaded) then return end
 
 -- local actions = require('telescope.actions')
 local themes = require('telescope.themes')
-
+local map = vim.keymap.set
 telescope.setup()
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>p', function() builtin.find_files({
+map('n', '<leader>p', function() builtin.find_files({
     theme = 'dropdown',
-    no_ignore = false,
     hidden = true,
   })
 end)
 
-vim.keymap.set('n', '<leader>f', function() builtin.grep_string({
-    theme = 'cursor',
+map('n', '<leader>f', function() builtin.grep_string() end)
 
-  })
-end)
-
-vim.keymap.set('n', '<leader>g', function()
+map('n', '<leader>g', function()
   themes.get_cursor()
   builtin.git_files()
 end)
 
-vim.keymap.set('n', '<leader>h', function() builtin.help_tags() end)
-vim.keymap.set('n', '<leader>r', function() builtin.resume() end)
+map('n', '<leader>h', function() builtin.help_tags() end)
+map('n', '<leader>r', function() builtin.resume() end)
 
 -- extension: file browser
 local function buffer_dir()
@@ -36,7 +31,7 @@ local function buffer_dir()
 end
 
 telescope.load_extension('file_browser')
-vim.keymap.set('n', '<leader>s', function()
+map('n', '<leader>s', function()
   -- local fb_actions = require 'telescope'.extensions.file_browser.actions
   telescope.extensions.file_browser.file_browser({
     theme = 'ivy',
