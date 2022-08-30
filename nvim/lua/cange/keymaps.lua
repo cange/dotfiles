6d66750d05ad30ae4730de4136e5ea01f8ca8920
Christian Angermann <psykmedia@gmail.com>
2022-08-30 23:59:29 +0200

    feat(neovim): align keymaps

--- nvim/lua/cange/keymaps.lua
+++ nvim/lua/cange/keymaps.lua
@@ -1,54 +1,53 @@
-local map = vim.keymap.set
+local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Do not yank x
-map('n', 'x', '"_x', opts)
+keymap('n', 'x', '"_x', opts)
 
 -- sort line
-map('v', '<F5>', ':sort<CR>gv=gv', opts)
+keymap('v', '<F5>', ':sort<CR>gv=gv', opts)
 
 -- Increment/decrement
-map('n', '+', '<C-a>', opts)
-map('n', '-', '<C-x>', opts)
+keymap('n', '+', '<C-a>', opts)
+keymap('n', '-', '<C-x>', opts)
 
--- map('n', 'dw', 'vb"_d',opts) -- Delete a word backwards
-map('v', 'p', '"_dP', opts) -- Keep clipboard content instead of overriding it
+keymap('v', 'p', '"_dP', opts) -- Keep clipboard content instead of overriding it
 
--- Select all
-map('n', '<C-a>', 'gg<S-v>G', opts)
+-- Select all content
+keymap('n', '<C-a>', 'gg<S-v>G', opts)
 
 -- New tab "ctrl+w + t"
-map('n', '<C-w>t', ':tabedit<CR>', opts)
+keymap('n', '<C-w>t', ':tabedit<CR>', opts)
 
 -- Move window "w+<direction>"
-map('', 'w<down>', '<C-w>j', opts)
-map('', 'w<left>', '<C-w>h', opts)
-map('', 'w<right>', '<C-w>l', opts)
-map('', 'w<up>', '<C-w>k', opts)
-map('', 'wh', '<C-w>h', opts)
-map('', 'wj', '<C-w>j', opts)
-map('', 'wk', '<C-w>k', opts)
-map('', 'wl', '<C-w>l', opts)
+keymap('', 'w<down>', '<C-w>j', opts)
+keymap('', 'w<left>', '<C-w>h', opts)
+keymap('', 'w<right>', '<C-w>l', opts)
+keymap('', 'w<up>', '<C-w>k', opts)
+keymap('', 'wh', '<C-w>h', opts)
+keymap('', 'wj', '<C-w>j', opts)
+keymap('', 'wk', '<C-w>k', opts)
+keymap('', 'wl', '<C-w>l', opts)
 
 -- Resize widow swith arrows
-map('n', '<C-down>', ':resize +4<CR>', opts)
-map('n', '<C-left>', ':vertical resize -4<CR>', opts)
-map('n', '<C-right>', ':vertical resize +4<CR>', opts)
-map('n', '<C-up>', ':resize -4<CR>', opts)
+keymap('n', '<C-down>', ':resize +4<CR>', opts)
+keymap('n', '<C-left>', ':vertical resize -4<CR>', opts)
+keymap('n', '<C-right>', ':vertical resize +4<CR>', opts)
+keymap('n', '<C-up>', ':resize -4<CR>', opts)
 
 -- Moving lines up and down
-map('i', '<A-down>', '<Esc>:move .+1<CR>==gi', opts)
-map('i', '<A-up>', '<Esc>:move .-2<CR>==gi', opts)
-map('n', '<A-down>', ':move .+1<CR>==', opts)
-map('n', '<A-up>', ':move .-2<CR>==', opts)
-map('v', '<A-down>', ":move '>+1<CR>gv=gv", opts)
-map('v', '<A-up>', ":move '<-2<CR>gv=gv", opts)
+keymap('i', '<A-down>', '<Esc>:move .+1<CR>==gi', opts)
+keymap('i', '<A-up>', '<Esc>:move .-2<CR>==gi', opts)
+keymap('n', '<A-down>', ':move .+1<CR>==', opts)
+keymap('n', '<A-up>', ':move .-2<CR>==', opts)
+keymap('v', '<A-down>', ":move '>+1<CR>gv=gv", opts)
+keymap('v', '<A-up>', ":move '<-2<CR>gv=gv", opts)
 
 -- switch between the last recent open two files
-map('n', '<leader><leader>', '<C-^>', opts)
+keymap('n', '<leader><leader>', '<C-^>', opts)
 
 -- Keep selection marked when indenting
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
-map('v', '<Tab>', '>gv', opts)
-map('v', '<S-Tab>', '<gv', opts)
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
+keymap('v', '<Tab>', '>gv', opts)
+keymap('v', '<S-Tab>', '<gv', opts)
