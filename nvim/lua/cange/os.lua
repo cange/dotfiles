d298935e752401cb768a7fabde42680f10f92413
Christian Angermann <psykmedia@gmail.com>
2022-09-03 17:17:12 +0200

    refactor(neovim): use nvim’s file structure as blueprint
    use https://github.com/nanotee/nvim-lua-guide#modules a

---
+++ nvim/lua/cange/os.lua
@@ -0,0 +1,16 @@
+-- Operation system related settings
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has 'macunix'
+local is_unix = has 'unix'
+local is_win = has 'win32'
+
+if is_mac then
+  vim.opt.clipboard:append { 'unnamedplus' }
+elseif is_unix then
+  print('unix setup loaded - linux.lua')
+elseif is_win then
+  vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+end
