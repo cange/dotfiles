cd394eabe8311f993b9618e9b3d76a0703127e3c
Christian Angermann <psykmedia@gmail.com>
2022-09-05 23:44:47 +0200

    feat(neovim): improve telescope set up

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,28 +2,33 @@ local ok, telescope = pcall(require, 'telescope')
 if not ok then return end
 
 -- local actions = require('telescope.actions')
-local themes = require('telescope.themes')
-local map = vim.keymap.set
-telescope.setup()
-
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+local keymap = vim.keymap.set
+local picker_opts = {
+  theme = 'dropdown',
+  previewer = false,
+}
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-n>'] = actions.cycle_history_next,
+        ['<C-p>'] = actions.cycle_history_prev,
+      },
+    },
+  },
+  pickers = {
+    find_files = picker_opts,
+    grep_string = picker_opts,
+  }
+})
 
-map('n', '<leader>p', function() builtin.find_files({
-    theme = 'dropdown',
-    hidden = true,
-  })
-end)
-
-map('n', '<leader>f', function() builtin.grep_string() end)
-
-map('n', '<leader>g', function()
-  themes.get_cursor()
-  builtin.git_files()
-end)
-
-map('n', '<leader>h', function() builtin.help_tags() end)
-map('n', '<leader>r', function() builtin.resume() end)
-map('n', '<leader>f', function() builtin.live_grep() end)
+keymap('n', '<leader>p', function() builtin.find_files() end)
+keymap('n', '<leader>h', function() builtin.help_tags() end)
+keymap('n', '<leader>lv', function() builtin.live_grep() end)
+keymap('n', '<leader>gs', function() builtin.grep_string() end)
+keymap('n', '<leader>g', function() builtin.git_commits() end)
 
 -- extension: file browser
 local function buffer_dir()
@@ -31,7 +36,7 @@ local function buffer_dir()
 end
 
 telescope.load_extension('file_browser')
-map('n', '<leader>s', function()
+keymap('n', '<leader>s', function()
   -- local fb_actions = require 'telescope'.extensions.file_browser.actions
   telescope.extensions.file_browser.file_browser({
     theme = 'ivy',
