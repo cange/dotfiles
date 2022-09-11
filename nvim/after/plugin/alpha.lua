f41341c6a4c6a9279e43c76ff044350a3277ad1d
Christian Angermann <psykmedia@gmail.com>
2022-09-12 01:30:54 +0200

    fix(neovim): day time greetings on start screen

--- nvim/after/plugin/alpha.lua
+++ nvim/after/plugin/alpha.lua
@@ -27,16 +27,16 @@ if not found_greetings then
 end
 
 section.buttons.val = {
-  button('f', icons.ui.Search ..        '  Find file',        ':Telescope find_files<CR>'),
-  button('p', icons.git.Repo ..         '  Find project',     ':lua require("telescope").extensions.projects.projects()<CR>'),
-  button('h', icons.ui.History ..       '  Recent files',     ':Telescope oldfiles <CR>'),
-  button('r', icons.ui.History ..       '  Recent session',   '<cmd>RestoreSession<CR>'),
-  button('e', icons.ui.NewFile ..       '  New file',         ':ene <BAR> startinsert <CR>'),
-  button('s', icons.ui.SignIn ..        '  Find session',     ':SearchSession<CR>'),
-  button('t', icons.ui.List ..          '  Find text',        ':Telescope live_grep <CR>'),
-  button('c', icons.ui.Gear ..          '  Config',           ':e ~/.config/nvim/init.lua <CR>'),
-  button('u', icons.ui.CloudDownload .. '  Update plugin',     ':PackerSync<CR>'),
-  button('q', icons.ui.SignOut ..       '  Quit',             ':qa<CR>'),
+  button('r', icons.ui.History ..       '  Recent session', '<cmd>RestoreSession<CR>'),
+  button('h', icons.ui.History ..       '  Recent files',   ':Telescope oldfiles <CR>'),
+  button('f', icons.ui.Search ..        '  Open file',                ':Telescope find_files<CR>'),
+  button('p', icons.git.Repo ..         '  Open project',        ':lua require("telescope").extensions.projects.projects()<CR>'),
+  button('e', icons.ui.NewFile ..       '  New file',            ':ene <BAR> startinsert <CR>'),
+  button('s', icons.ui.SignIn ..        '  Open session',        ':SearchSession<CR>'),
+  button('t', icons.ui.List ..          '  Find text',           ':Telescope live_grep <CR>'),
+  button('c', icons.ui.Gear ..          '  Config',              ':e ~/.config/nvim/init.lua <CR>'),
+  button('u', icons.ui.CloudDownload .. '  Update plugin',       ':PackerSync<CR>'),
+  button('q', icons.ui.SignOut ..       '  Quit',                ':qa<CR>'),
 }
 
 section.footer.val = greetings.random_with_name(meta.author.display_name)
--- nvim/lua/cange/utils/greetings.lua
+++ nvim/lua/cange/utils/greetings.lua
@@ -19,47 +19,70 @@ end
 local function greeting_by_day_time()
   local h = tonumber(vim.fn.strftime('%k', vim.fn.localtime()))
   local day_time = 'morning'
-  if h > 12 or h < 18 then day_time = 'afternoon'
-  elseif h < 5 or h > 18 then day_time = 'evening'
+  if h > 12 and h <= 18 then day_time = 'afternoon'
+  elseif h >= 18 or h < 5 then day_time = 'evening'
   end
   return 'Good '..day_time
 end
 
---- @table greetings
-local greeting_by_day_time_with_name = greeting_by_day_time()..' ${name}!'
-local greetings = {
-  'What’s going on ${name}?',
-  '${name}, its been a while!',
-  'Good to see you, ${name}!',
-  'Hello ${name}!',
-  'Hey ${name}!',
-  'Howdy, ${name}!',
-  'Hi ${name}',
-  'How are you ${name}?',
-  'How’s it going ${name}?',
-  'Long time no see ${name}!',
-  'Nice to see you ${name}!',
-  'Sup ${name}?',
-  'Wazzup ${name}?',
-  'Yo ${name}!',
-  greeting_by_day_time_with_name, -- prio this greeting by adding it multipe times
-  greeting_by_day_time_with_name,
-  greeting_by_day_time_with_name,
-  greeting_by_day_time_with_name,
-  greeting_by_day_time_with_name,
-}
+---Repeat string as many as given count
+---@param str string
+---@param count integer
+---@return table<string>
+local function repeat_string(str, count)
+  local i = 1
+  local result = {}
+  repeat
+    result[i] = str;
+    i = i + 1
+  until(i > count)
+  return result
+end
+
+---Generate senctence dependent ont given weight
+---@param senctence_and_weight table<string, integer>
+---@return table<string> List of sentences
+local function generate_by_weight(senctence_and_weight)
+  local s = {}
+  for _, pair in pairs(senctence_and_weight) do
+    s = vim.list_extend(s, repeat_string(pair[1], pair[2]))
+  end
+  return s
+end
+
+---List of greetings with the amount corresponding to the weight
+---@return table<string>
+local function greetings_and_weights()
+  return generate_by_weight({
+    { 'What’s going on ${name}?', 1 },
+    { '${name}, its been a while!', 1 },
+    { 'Good to see you, ${name}!', 1 },
+    { 'Hello ${name}!', 1 },
+    { 'Hey ${name}!', 1 },
+    { 'Howdy, ${name}!', 1 },
+    { 'Hi ${name}', 1 },
+    { 'How are you ${name}?', 1 },
+    { 'How’s it going ${name}?', 1 },
+    { 'Long time no see ${name}!', 1 },
+    { 'Nice to see you ${name}!', 1 },
+    { 'Sup ${name}?', 1 },
+    { 'Wazzup ${name}?', 1 },
+    { 'Yo ${name}!', 1 },
+    { greeting_by_day_time()..' ${name}!', 6 }, -- prio this greeting by adding it multipe times
+  })
+end
+
+---Generates random gretting with given name and day time.
+--- @param name string
+--- @return string
+local function random_with_name(name)
+  local greetings = greetings_and_weights()
+  local greeting = greetings[random_range(1, vim.tbl_count(greetings))]
+  return interp(greeting, { name = name })
+end
 
 ---Provides greeting functionalities.
 ---@class Greetings
 local M = {}
-
----Generates random gretting with given name and day time.
---- @type function
---- @param name string
---- @return string
-M.random_with_name = function(name)
-  local count = vim.tbl_count(greetings)
-  return interp(greetings[random_range(1, count)], { name = name })
-end
-
+M.random_with_name = random_with_name
 return M
