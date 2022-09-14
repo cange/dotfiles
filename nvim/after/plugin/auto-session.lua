9a1fa6962edd4fc2df40bf99d0eeeb61559a5787
Christian Angermann <christian.angermann@visable.com>
2022-09-14 22:57:10 +0200

    feat(neovim): increase mode contrast

--- nvim/after/plugin/auto-session.lua
+++ nvim/after/plugin/auto-session.lua
@@ -15,27 +15,41 @@ if not found_lens then
   return
 end
 
-local opts = {
+local found_lualine, lualine = pcall(require, 'lualine')
+vim.notify('auto-session: "lualine" could not be found')
+if not found_lualine then
+  return
+end
+
+local settings = {
   log_level = 'info',
-  auto_session_enable_last_session = false,
-  auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
-  auto_session_enabled = true,
-  auto_save_enabled = nil,
-  auto_restore_enabled = nil,
-  auto_session_suppress_dirs = { '~/', '~/workspace' },
-  auto_session_use_git_branch = nil,
-  -- the configs below are lua only
-  -- bypass_session_save_file_types = { 'alpga' },
+  -- auto_restore_enabled = nil, -- Enables/disables auto restoring
+  -- auto_save_enabled = nil, -- Enables/disables auto saving
+  -- auto_session_allowed_dirs = nil, -- Allow session create/restore if in one of the list of dirs
+  -- auto_session_create_enabled = true, -- Enables/disables the plugin's session auto creation
+  auto_session_enable_last_session = true, -- Loads the last loaded session if session for cwd does not exist
+  -- auto_session_enabled = true, -- Enables/disables the plugin's auto save and restore features
+  -- auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/', -- Changes the root dir for sessions
+  auto_session_suppress_dirs = { '~/', '~/workspace' }, -- Suppress session create/restore if in one of the list of dirs
+  auto_session_use_git_branch = nil, -- Use the git branch to differentiate the session name
+
+  cwd_change_handling = {
+    -- restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+    -- pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+    post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+      lualine.refresh() -- refresh lualine so the new session name is displayed in the status bar
+    end,
+  },
 }
 
+-- better experience with the plugin overall using this config for sessionoptions is recommended.
 vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal'
 
 telescope.load_extension('session-lens')
-
 session_lens.setup({
   path_display = { 'shorten' },
   previewer = true,
   prompt_title = 'Sessions',
 })
 
-auto_session.setup(opts)
+auto_session.setup(settings)
--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -8,8 +8,42 @@ if not found_auto_session then
   vim.notify('lualine: "auto-session-library" could not be found')
   return
 end
-local current_sections = lualine.get_config().sections
 
+local current_sections = lualine.get_config().sections
+local theme = lualine.get_config().options.theme
+
+local found_theme, theme_palette = pcall(require, 'nightfox.palette')
+if found_theme then
+  local color = theme_palette.load('terafox')
+  -- vim.pretty_print(color.green)
+  -- vim.pretty_print(vim.tbl_keys(color))
+  theme = {
+    normal = {
+      a = { bg = color.bg4, fg = color.fg2, gui = 'bold' },
+      b = { bg = color.bg3, fg = color.fg0 },
+      c = { bg = color.bg0, fg = color.fg1 },
+    },
+    insert = {
+      a = { bg = color.green.bright, fg = color.bg1, gui = 'bold' },
+    },
+    visual = {
+      a = { bg = color.pink.bright, fg = color.bg1, gui = 'bold' },
+    },
+    replace = {
+      a = { bg = color.red.base, fg = color.fg3, gui = 'bold' },
+    },
+    command = {
+      a = { bg = color.yellow.base, fg = color.fg3, gui = 'bold' },
+    },
+    inactive = {
+      a = { bg = color.bg1, fg = color.bg4, gui = 'bold' },
+    },
+  }
+end
 lualine.setup({
-  sections = vim.tbl_extend('keep', current_sections, { lualine_w = { auto_session.current_session_name } }),
+  sections = vim.tbl_extend('keep', current_sections, { lualine_d = { auto_session.current_session_name } }),
+  options = {
+    -- theme = vim.tbl_extend('keep', theme, current_theme),
+    theme = theme,
+  },
 })
