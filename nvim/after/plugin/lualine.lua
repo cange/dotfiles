96daed5351479eefe448f04ac5903ac36d74fa75
Christian Angermann <christian.angermann@visable.com>
2022-09-15 00:54:21 +0200

    feat(neovim): update lualine icons set

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,13 +3,17 @@ if not found_lualine then
   return
 end
 
+local found_icons, icons = pcall(require, 'cange.icons')
+if not found_icons then
+  vim.notify('lualine: "cange.icons" could not be found')
+  return
+end
 local found_auto_session, auto_session = pcall(require, 'auto-session-library')
 if not found_auto_session then
   vim.notify('lualine: "auto-session-library" could not be found')
   return
 end
 
-local current_sections = lualine.get_config().sections
 local theme = lualine.get_config().options.theme
 
 local found_theme, theme_palette = pcall(require, 'nightfox.palette')
@@ -25,6 +29,7 @@ if found_theme then
     },
     insert = {
       a = { bg = color.green.bright, fg = color.bg1, gui = 'bold' },
+      b = { bg = color.green.dim, fg = color.b0 },
     },
     visual = {
       a = { bg = color.pink.bright, fg = color.bg1, gui = 'bold' },
@@ -41,9 +46,24 @@ if found_theme then
   }
 end
 lualine.setup({
-  sections = vim.tbl_extend('keep', current_sections, { lualine_d = { auto_session.current_session_name } }),
-  options = {
-    -- theme = vim.tbl_extend('keep', theme, current_theme),
-    theme = theme,
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        -- file_status = true, -- Displays file status (readonly status, modified status)
+        -- newfile_status = false, -- Display new file status (new file means no write after created)
+        path = 1,
+        -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = icons.lualine,
+      },
+      -- lualine_d = { auto_session.current_session_name() },
+    },
   },
+  options = { theme = theme },
 })
