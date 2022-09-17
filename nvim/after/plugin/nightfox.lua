eb64e02d608f57e65c93fd169529d2141102835e
Christian Angermann <christian.angermann@visable.com>
2022-09-17 15:39:14 +0200

    refactor(colorscheme): establish dedicated module for theming

--- nvim/after/plugin/nightfox.lua
+++ nvim/after/plugin/nightfox.lua
@@ -1,25 +1,25 @@
 local found_colorscheme, colorscheme = pcall(require, 'cange.colorscheme')
 if not found_colorscheme then
-  print('nightfox: "cange.colorscheme" module not found')
+  print('[nightfox] "cange.colorscheme" module not found')
   return
 end
 
 -- theme https://github.com/EdenEast/nightfox.nvim#usage
 local found, palette = pcall(require, colorscheme.theme .. '.palette')
 if not found then
-  print('nightfox: colorscheme "' .. colorscheme.theme .. '.palette" module not found')
+  print('[nightfox] colorscheme "' .. colorscheme.theme .. '.palette" module not found')
   return
 end
 
 local found_theme, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme.variation)
 if not found_theme then
-  print('nightfox: "' .. colorscheme.variation .. '" of "' .. colorscheme.theme .. '" colorscheme not found!')
+  print('[nightfox] "' .. colorscheme.variation .. '" of "' .. colorscheme.theme .. '" colorscheme not found!')
   return
 end
 
 local found_modes, modes = pcall(require, 'modes')
 if not found_modes then
-  print('nightfox: "modes" module not found')
+  print('[nightfox] "modes" module not found')
   return
 end
 -- needs to been calld after colorscheme is established
