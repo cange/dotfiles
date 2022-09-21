f6b1cd9266bf43a83f7dd3773c81a7695096451e
Christian Angermann <christian.angermann@visable.com>
2022-09-21 22:18:45 +0200

    feat(nvim): reduce diagnostics reports within UI

--- nvim/lua/cange/lsp/init.lua
+++ nvim/lua/cange/lsp/init.lua
@@ -1,17 +1,7 @@
 _G.bulk_loader('lsp', {
+  { 'cange.lsp.diagnostics', '_' },
   { 'cange.lsp.mason', 'mason' }, --[[ 1 ]]
   { 'cange.lsp.mason-lspconfig', 'mason_lspconfig' }, --[[ 2 ]]
   { 'cange.lsp.lspconfig', 'lspconfig' }, --[[ 3 ]]
   { 'cange.lsp.null-ls', 'null_ls' }, --[[ 4 ]]
 })
-local utils = _G.bulk_loader('lsp', { { 'cange.icons', 'icons' } })
-local signs = {
-  { name = 'DiagnosticSignError', text = utils.icons.diagnostics.Error },
-  { name = 'DiagnosticSignWarn', text = utils.icons.diagnostics.Warning },
-  { name = 'DiagnosticSignHint', text = utils.icons.diagnostics.Hint },
-  { name = 'DiagnosticSignInfo', text = utils.icons.diagnostics.Information },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
-end
