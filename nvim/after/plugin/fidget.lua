80654bd9f39e5b2c2e60bab435e3b0bfc0b68d34
Christian Angermann <christian.angermann@visable.com>
2022-09-22 21:05:15 +0200

    refactor(nvim): cleanup fidget setup

--- nvim/after/plugin/fidget.lua
+++ nvim/after/plugin/fidget.lua
@@ -3,21 +3,8 @@ if not found_fidget then
   return
 end
 
-local utils = _G.bulk_loader('utils', { { 'cange.icons', 'icons' } })
-local function spinner_by_day_time()
-  local h = tonumber(vim.fn.strftime('%k', vim.fn.localtime()))
-  return h > 8 and h < 20 and 'earth' or 'moon'
-end
-
 fidget.setup({
   text = {
-    spinner = spinner_by_day_time(),
-    done = utils.icons.ui.Check,
-  },
-  align = {
-    bottom = true,
-  },
-  window = {
-    relative = 'editor',
+    spinner = 'dots',
   },
 })
