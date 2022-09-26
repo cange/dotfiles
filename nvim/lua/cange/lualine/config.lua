5192a617aad97bc1ba93a7ff67539503ce429261
Christian Angermann <christian.angermann@visable.com>
2022-09-27 00:41:35 +0200

    feat(nvim): indicate active LSPs WIP

---
+++ nvim/lua/cange/lualine/config.lua
@@ -0,0 +1,17 @@
+---@enumn separators
+local separators = {
+  arrow_strong = { left = '', right = '' },
+  arrow_subtle = { left = '', right = '' },
+  pill_strong = { left = '', right = '' },
+  pill_subtle = { left = '', right = '' },
+  strong = { left = '⏽', right = '⏽' },
+  subtle = { left = '│', right = '│' },
+  none = { left = '', right = '' },
+}
+
+---@enum config
+local M = {
+  separators = separators,
+}
+
+return M
