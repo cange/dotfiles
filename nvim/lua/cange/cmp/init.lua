369e25b97771635353f6f186a728df095cd7dec4
Christian Angermann <christian.angermann@visable.com>
2022-09-27 00:40:23 +0200

    feat(nvim): enjoy snippets with choices WIP

--- nvim/lua/cange/cmp/init.lua
+++ nvim/lua/cange/cmp/init.lua
@@ -17,11 +17,20 @@ if not found_config then
   print('[cmp.init] "cange.cmp.config" not found')
   return
 end
+local found_methods, custom_methods = pcall(require, 'cange.cmp.methods')
+if not found_methods then
+  print('[cmp.init] "cange.cmp.methods" not found')
+  return
+end
+local found_luasnip, luasnip = pcall(require, 'luasnip')
+if not found_luasnip then
+  print('[cmp.init] "luasnip" not found')
+  return
+end
 local function keymap(lhs, rhs)
   vim.keymap.set({ 'i', 's' }, lhs, rhs, { noremap = true, silent = true })
 end
 
-local mapping = cmp.mapping
 -- config
 keymap('<C-[>', '<Plug>luasnip-prev-choice')
 keymap('<C-]>', '<Plug>luasnip-next-choice')
@@ -29,25 +38,71 @@ keymap('<C-up>', '<Plug>luasnip-prev-choice')
 keymap('<C-down>', '<Plug>luasnip-next-choice')
 
 local settings = vim.tbl_extend('keep', config.props, {
-  mapping = {
-    ['<C-Space>'] = mapping(mapping.complete(), { 'i', 'c' }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-up>'] = cmp.mapping.select_prev_item(),
+    ['<C-down>'] = cmp.mapping.select_next_item(),
     ['<C-s>'] = mapping(mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-a>'] = mapping(mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<CR>'] = mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<ESC>'] = mapping({
-      i = mapping.abort(),
-      c = mapping.close(),
+    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+    ['<C-y>'] = cmp.mapping({
+      i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+      c = function(fallback)
+        if cmp.visible() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
     }),
-    ['<C-e>'] = mapping({
-      i = mapping.abort(),
-      c = mapping.close(),
-    }), -- Accept currently selected item. If none selected, `select` first item.
-    ['<down>'] = mapping(config.next_item, { 'i', 's' }),
-    ['<Tab>'] = mapping(config.next_item, { 'i', 's' }),
-    ['<up>'] = mapping(config.prev_item, { 'i', 's' }),
-    ['<S-Tab>'] = mapping(config.prev_item, { 'i', 's' }),
-  },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif custom_methods.jumpable(1) then
+        luasnip.jump(1)
+      elseif custom_methods.has_words_before() then
+        -- cmp.complete()
+        fallback()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        local confirm_opts = { -- avoid mutating the original opts below
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }
+        local is_insert_mode = function()
+          return vim.api.nvim_get_mode().mode:sub(1, 1) == 'i'
+        end
+        if is_insert_mode() then -- prevent overwriting brackets
+          confirm_opts.behavior = cmp.ConfirmBehavior.Insert
+        end
+        if cmp.confirm(confirm_opts) then
+          return -- success, exit early
+        end
+      end
+
+      if custom_methods.jumpable(1) and luasnip.jump(1) then
+        return -- success, exit early
+      end
+      fallback() -- if not exited early, always fallback
+    end),
+  }),
 })
 
 cmp.setup(settings)
