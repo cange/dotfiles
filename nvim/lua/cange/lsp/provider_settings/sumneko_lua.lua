219cb80f235a9f5fdf72294eef64b609ea92723e
Christian Angermann <christian.angermann@visable.com>
2022-10-05 00:32:43 +0200

    refactor(nvim): make LSP for lua bit more performance

--- nvim/lua/cange/lsp/provider_settings/sumneko_lua.lua
+++ nvim/lua/cange/lsp/provider_settings/sumneko_lua.lua
@@ -2,6 +2,18 @@
 return {
   settings = {
     Lua = {
+      format = {
+        enable = false,
+      },
+      hint = {
+        enable = true,
+        arrayIndex = 'Disable', -- "Enable", "Auto", "Disable"
+        await = true,
+        paramName = 'Disable', -- "All", "Literal", "Disable"
+        paramType = false,
+        semicolon = 'Disable', -- "All", "SameLine", "Disable"
+        setType = true,
+      },
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
@@ -18,7 +30,13 @@ return {
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.stdpath('config') .. '/lua'] = true,
+          -- [vim.fn.datapath "config" .. "/lua"] = true,
+          -- Make the server aware of Neovim runtime files
+          -- [vim.api.nvim_get_runtime_file('', true)] = true,
+        },
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
