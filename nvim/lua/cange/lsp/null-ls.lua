d63628e7afbb95896ae6c3a113aa746363de3479
Christian Angermann <christian.angermann@visable.com>
2022-10-11 22:19:48 +0200

    feat(nvim): boost null-ls performance

--- nvim/lua/cange/lsp/null-ls.lua
+++ nvim/lua/cange/lsp/null-ls.lua
@@ -7,20 +7,28 @@ end
 -- config
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 local formatting = null_ls.builtins.formatting
 -- local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
+  update_in_insert = false, -- if true, it runs diagnostics in insert mode, which impacts performance
   sources = {
     formatting.stylua,
-    formatting.prettierd, -- pretty fast prettier
+    formatting.prettierd.with({ -- pretty fast prettier
+      command = vim.fn.expand("$HOME/.asdf/shims/prettierd"),
+    }),
+    formatting.stylelint,
+    formatting.eslint_d.with({
+      command = vim.fn.expand("$HOME/.asdf/shims/eslint_d"),
+    }),
 
     diagnostics.stylelint,
-    diagnostics.eslint_d,
+    diagnostics.eslint_d.with({
+      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    }),
     diagnostics.jsonlint,
 
-    -- formatting.stylelint,
-    -- formatting.eslint_d,
-    -- code_actions.eslint_d,
+    code_actions.eslint_d,
   },
 })
