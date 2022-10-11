666f24b9c78eb0726bb87ecc30fbca2fd55fa635
Christian Angermann <christian.angermann@visable.com>
2022-10-11 22:30:31 +0200

    feat(nvim): null-ls format on save

--- nvim/lua/cange/lsp/null-ls.lua
+++ nvim/lua/cange/lsp/null-ls.lua
@@ -11,6 +11,15 @@ local code_actions = null_ls.builtins.code_actions
 local formatting = null_ls.builtins.formatting
 -- local code_actions = null_ls.builtins.code_actions
 
+local function lsp_formatting(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
 null_ls.setup({
   update_in_insert = false, -- if true, it runs diagnostics in insert mode, which impacts performance
   sources = {
@@ -31,4 +40,17 @@ null_ls.setup({
 
     code_actions.eslint_d,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      local augroup = vim.api.nvim_create_augroup("CangeLspFormatting", {})
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end,
 })
