04b96428a0bdc9fd7aaf11d06adf288610b4648d
Christian Angermann <christian.angermann@visable.com>
2022-11-02 19:53:38 +0100

    chore(nvim): remove obsolete files

--- nvim/after/plugin/vim-visual-multi.lua
+++
@@ -1,6 +0,0 @@
-local ns = "[after/plugin/vim-visual-multi]"
-local found_vim_visual_multi, vim_visual_multi = pcall(require, "vim-visual-multi")
-if not found_vim_visual_multi then
-  print(ns, '"vim-visual-multi" not found')
-  return
-end
