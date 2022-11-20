3d4433089d062e96d894e47747f60f5e74e5a4ad
Christian Angermann <christian.angermann@visable.com>
2022-11-20 17:32:44 +0100

    feat(nvim): simplify cmp setup

--- nvim/lua/cange/cmp/init.lua
+++ nvim/lua/cange/cmp/init.lua
@@ -10,19 +10,14 @@ if not found_luasnip then
   print(ns, '"luasnip" not found')
   return
 end
-local found_config_luasnip, config_luasnip = pcall(require, "cange.cmp.luasnip")
-if not found_config_luasnip then
-  print(ns, '"cange.cmp.luasnip" not found')
+local found_cmp_utils, cmp_utils = pcall(require, "cange.cmp.utils")
+if not found_cmp_utils then
+  print(ns, '"cange.cmp.utils" not found')
   return
 end
 local found_utils, utils = pcall(require, "cange.utils")
 if not found_utils then
   print(ns, '"cange.utils" not found')
-  return
-end
-local found_cmp_utils, cmp_utils = pcall(require, "cange.cmp.utils")
-if not found_cmp_utils then
-  print(ns, '"cange.cmp.utils" not found')
   return
 end
 
@@ -39,49 +34,52 @@ local function menu_item_format(entry, vim_item)
   return vim_item
 end
 
+-- Setup
 vim.opt.completeopt = { "menu", "menuone", "noselect" } -- enable Insert mode completion
 
--- Setup
-config_luasnip.setup()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets" })
+require("luasnip.loaders.from_vscode").lazy_load()
+
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    ["<C-s>"] = cmp.mapping.scroll_docs(4),
     ["<C-a>"] = cmp.mapping.scroll_docs(-4),
-
-    -- Cancellations
-    ["<C-c>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
-
-    -- Confirmations
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- `false` to confirm explicitly selected items only
+    ["<C-s>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
 
     -- Choice
-    ["<C-]>"] = cmp.mapping(function(fallback)
+    ["<C-n>"] = cmp.mapping(function()
       if luasnip.choice_active() then
-        print("has choice")
         luasnip.change_choice(1)
       else
-        fallback()
+        cmp.mapping.select_next_item()
       end
     end, { "i" }),
-    ["<C-[>"] = cmp.mapping(function(fallback)
+    ["<C-p>"] = cmp.mapping(function()
       if luasnip.choice_active() then
         luasnip.change_choice(-1)
       else
-        fallback()
+        cmp.mapping.select_prev_item()
       end
     end, { "i" }),
 
-    -- Selection
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-c>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif cmp_utils.jumpable(1) then
-        luasnip.jump(1)
       elseif cmp_utils.has_words_before() then
-        cmp.mapping.complete()
+        fallback()
       else
         fallback()
       end
