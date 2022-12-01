f452e4e2e9f2d08d019634b2340f4f637f511c37
Christian Angermann <christian.angermann@visable.com>
2022-12-01 23:10:41 +0100

    fix(nvim): cmp cmdline keymaps tab completion

--- nvim/lua/cange/cmp/init.lua
+++ nvim/lua/cange/cmp/init.lua
@@ -120,6 +120,7 @@ cmp.setup.cmdline("/", {
 
 -- `:` cmdline setup.
 cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, {
