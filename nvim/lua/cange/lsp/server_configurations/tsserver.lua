00241d74073be0a60f176dd337c76a5c7d2ddc8d
Christian Angermann <christian.angermann@visable.com>
2022-12-20 16:43:02 +0100

    perf(nvim): avoid starting tsserver on plain JS files

---
+++ nvim/lua/cange/lsp/server_configurations/tsserver.lua
@@ -0,0 +1,12 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/tsserver.lua
+--
+return {
+  filetypes = {
+    -- 'javascript', // handled by eslint
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+}
