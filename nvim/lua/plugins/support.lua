50dc95b486cc5abf1e1769a74c049c4fc7ff6bdc
Christian Angermann <christian.angermann@visable.com>
2023-02-17 12:10:37 +0100

    feat(nvim): open markdown previews in browser

--- nvim/lua/plugins/support.lua
+++ nvim/lua/plugins/support.lua
@@ -140,7 +140,7 @@ return {
         syntax = true, -- enable syntax highlighting, affects performance
         theme = "dark", -- 'dark' or 'light'
         update_on_change = true,
-
+        app = "browser", -- open in target 'webview', 'browser'
         -- relevant if update_on_change is true
         throttle_at = 200000, -- start throttling when file exceeds this amount of bytes in size
         throttle_time = "auto", -- minimum amount of time in milliseconds that has to pass before starting new render
