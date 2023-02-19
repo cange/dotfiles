237fcb1963412f1bf55d280ec529ee8dfb8dd656
Christian Angermann <christian.angermann@visable.com>
2023-02-19 14:51:14 +0100

    fix(shell): redefine PATH
    - use custom git (homebrew)
    - enable ifconfig

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,5 +1,5 @@
 # User configuration
-export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=/usr/local/bin:/usr/local/sbin:/sbin/:$PATH
 # export MANPATH="/usr/local/man:$MANPATH"
 
 # One may need to manually set your language environment
@@ -10,7 +10,7 @@ export LANG=en_US.UTF-8
 export SPACESHIP_CONFIG_FILE="$HOME/.config/zsh/prompt.zsh"
 
 # yarn - node package manager
-export PATH="$PATH:`yarn global bin`"
+export PATH="`yarn global bin`:$PATH"
 
 # To link Rubies to Homebrew's OpenSSL 1.1
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
@@ -31,14 +31,14 @@ export BROWSER="firefox"
 # pnpm
 if [[ -s "${HOME}/Library/pnpm" ]]; then
   export PNPM_HOME="${HOME}/Library/pnpm"
-  export PATH="$PATH:$PNPM_HOME"
+  export PATH="$PNPM_HOME:$PATH"
 fi
 # pnpm end
 
 # bun completions
 if [[ -s "${HOME}/.bun" ]]; then
   export BUN_INSTALL="${HOME}/.bun"
-  export PATH="$PATH:$BUN_INSTALL/bin"
+  export PATH="$BUN_INSTALL/bin:$PATH"
   source "${HOME}/.bun/_bun"
 fi
 # bun end
