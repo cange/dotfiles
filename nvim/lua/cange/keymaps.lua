2bbe0ef4bc5cdd552f711bb148bab6a3d22f1d7d
Christian Angermann <christian.angermann@visable.com>
2023-02-28 21:57:53 +0100

    feat(nvim): add description to common keymaps

--- nvim/lua/cange/keymaps.lua
+++ nvim/lua/cange/keymaps.lua
@@ -1,68 +1,48 @@
 -- local ns = "[cange.keymaps.common]"
 --
 local keymaps = {
-  -- Keep clipboard content instead of overriding i
-  { "v", "p", '"_dP' },
-
-  -- greatest remap ever
-  { "x", "<leader>p", '"_dP' },
-  --
-  -- -- next greatest remap ever : asbjornHaland
-  { { "n", "v" }, "<leader>y", '"+y' },
-  { { "n", "v" }, "<leader>d", '"_d' },
-  { "n", "<leader>Y", '"+Y', { noremap = false } },
-  --
-  -- Select all content
-  { "n", "<C-a>", "gg<S-v>G" },
-  --
-  -- New tab "ctrl+w + t"
-  { "n", "<C-w>t", ":tabedit<CR>" },
-
-  -- Move window "w+<direction>"
-  { "", "w<left>", "<C-w>h" },
-  { "", "w<down>", "<C-w>j" },
-  { "", "w<up>", "<C-w>k" },
-  { "", "w<right>", "<C-w>l" },
-  { "", "wh", "<C-w>h" },
-  { "", "wj", "<C-w>j" },
-  { "", "wk", "<C-w>k" },
-  { "", "wl", "<C-w>l" },
-
-  -- Moving lines up and down
-  { "n", "<A-down>", ":move .+1<CR>==" },
-  { "n", "<A-up>", ":move .-2<CR>==" },
-  { "x", "<A-down>", ":move'>+<CR>='[gv" },
-  { "x", "<A-up>", ":move-2<CR>='[gv" },
-  { "x", "j", ":move'>+<CR>='[gv" },
-  { "x", "k", ":move-2<CR>='[gv" },
-
-  { "n", "<leader>-", "<C-W>s", { desc = "Split window below" } },
-  { "n", "<leader>|", "<C-W>v", { desc = "Split window right" } },
-
-  --- Resize window with arrows
-  { "n", "<C-down>", ":resize +4<CR>" },
-  { "n", "<C-left>", ":vertical resize -4<CR>" },
-  { "n", "<C-up>", ":resize -4<CR>" },
-  { "n", "<C-right>", ":vertical resize +4<CR>" },
-
-  -- Keep selection marked when indenting
-  { "v", "<", "<gv" },
-  { "v", ">", ">gv" },
-  { "v", "<Tab>", ">gv" },
-  { "v", "<S-Tab>", "<gv" },
-
-  -- switch between the last recent open two files
-  { "n", "<leader><leader>", "<C-^>" },
-
-  -- Reload current file
+  { "v", "p", '"_dP', { desc = "Clipboard: keep content" } },
+  { "n", "<C-a>", "gg<S-v>G", { desc = "Select all content" } },
+  { "v", "<F5>", ":sort<CR>gv=gv", { desc = "Sort selected lines" } },
+  { "n", "<leader><leader>", "<C-^>", { desc = "Switch last recent 2 buffers" } },
   {
     "n",
     "<leader><leader>x",
     ':write<CR>:luafile %<CR>:lua Cange.log_info("File saved and executed", "Executed!")<CR>',
+    { desc = "Reload current file" },
   },
 
-  -- Sort lines
-  { "v", "<F5>", ":sort<CR>gv=gv" },
+  -- Move window scope
+  { "", "w<left>", "<C-w>h", { desc = "Window scope: left" } },
+  { "", "w<down>", "<C-w>j", { desc = "Window scope: down" } },
+  { "", "w<up>", "<C-w>k", { desc = "Window scope: up" } },
+  { "", "w<right>", "<C-w>l", { desc = "Window scope: right" } },
+  { "", "wh", "<C-w>h", { desc = "Window scope: left" } },
+  { "", "wj", "<C-w>j", { desc = "Window scope: down" } },
+  { "", "wk", "<C-w>k", { desc = "Window scope: up" } },
+  { "", "wl", "<C-w>l", { desc = "Window scope: right" } },
+
+  -- Moving lines up and down
+  { "n", "<A-down>", ":move .+1<CR>==", { desc = "Line move: down" } },
+  { "n", "<A-up>", ":move .-2<CR>==", { desc = "Line move: up" } },
+  { "x", "<A-down>", ":move'>+<CR>='[gv", { desc = "Line move: down" } },
+  { "x", "<A-up>", ":move-2<CR>='[gv", { desc = "Line move: up" } },
+  { "x", "j", ":move'>+<CR>='[gv", { desc = "Line move: down" } },
+  { "x", "k", ":move-2<CR>='[gv", { desc = "Line move: up" } },
+
+  { "n", "<leader>-", "<C-W>s", { desc = "Window split: below" } },
+  { "n", "<leader>|", "<C-W>v", { desc = "Window split: right" } },
+
+  { "n", "<C-down>", ":resize +4<CR>", { desc = "Window resize: down" } },
+  { "n", "<C-left>", ":vertical resize -4<CR>", { desc = "Window resize: left" } },
+  { "n", "<C-up>", ":resize -4<CR>", { desc = "Window resize: up" } },
+  { "n", "<C-right>", ":vertical resize +4<CR>", { desc = "Window resize: right" } },
+
+  -- Keep selection marked when indenting
+  { "v", "<", "<gv", { desc = "Indent: left" } },
+  { "v", ">", ">gv", { desc = "Indent: right" } },
+  { "v", "<Tab>", ">gv", { desc = "Indent: left" } },
+  { "v", "<S-Tab>", "<gv", { desc = "Indent: right" } },
 }
 
 for _, km in pairs(keymaps) do
