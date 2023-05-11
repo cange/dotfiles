f244cdcb58f662d5c9d7adb05daa9c5f9a42813e
Christian Angermann <christian.angermann@visable.com>
2023-05-11 21:56:20 +0200

    refactor(nvim): streamline notify setup

--- nvim/lua/cange/plugins/support.lua
+++ nvim/lua/cange/plugins/support.lua
@@ -2,47 +2,20 @@ return {
   { -- popover notification
     "rcarriga/nvim-notify",
     dependencies = { "nvim-telescope/telescope.nvim" },
-    opts = {
-      -- Animation style (see below for details)
-      stages = "fade_in_slide_out",
-
-      -- Function called when a new window is opened, use for changing win settings/config
-      on_open = nil,
-
-      -- Function called when a window is closed
-      on_close = nil,
-
-      -- Render function for notifications. See notify-render()
-      render = "default",
-
-      -- Default timeout for notifications
-      timeout = 175,
-
-      -- For stages that change opacity this is treated as the highlight behind the window
-      -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-      background_colour = "Normal",
-
-      -- Minimum width for notification windows
-      minimum_width = 10,
-
-      -- Icons for the different levels
-      icons = {
-        ERROR = Cange.get_icon("diagnostics.Error"),
-        WARN = Cange.get_icon("diagnostics.Warn"),
-        INFO = Cange.get_icon("diagnostics.Info"),
-        DEBUG = Cange.get_icon("ui.Bug"),
-        TRACE = Cange.get_icon("ui.Pencil"),
-      },
-    },
-    config = function(_, opts)
-      require("notify").setup(opts)
+    config = function()
+      require("notify").setup({
+        stages = "static",
+        background_colour = "FloatBorder",
+        icons = {
+          ERROR = Cange.get_icon("diagnostics.Error"),
+          WARN = Cange.get_icon("diagnostics.Warn"),
+          INFO = Cange.get_icon("diagnostics.Info"),
+          DEBUG = Cange.get_icon("ui.Bug"),
+          TRACE = Cange.get_icon("ui.Pencil"),
+        },
+      })
       require("telescope").load_extension("notify")
-
-      vim.notify = function(msg, ...)
-        if msg:match("character_offset must be called") or msg:match("method textDocument") then return end
-
-        require("notify")(msg, ...)
-      end
+      vim.notify = require("notify")
     end,
   },
 
