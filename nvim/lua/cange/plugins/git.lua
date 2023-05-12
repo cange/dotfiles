e1cb2e6bdec364a8ea4cfade1e318b1ee77fc5e7
Christian Angermann <christian.angermann@visable.com>
2023-05-13 00:01:24 +0200

    refactor(nvim): streamline gitsign config

--- nvim/lua/cange/plugins/git.lua
+++ nvim/lua/cange/plugins/git.lua
@@ -1,27 +1,21 @@
-local icon = Cange.get_icon
+local i = Cange.get_icon
+local line_format = "   " .. i("git.Commit") .. "   <author>, <author_time:%d.%m.%y> " .. i("ui.Note") .. " <summary>"
 
--- stylua: ignore start
 return {
   {
     "lewis6991/gitsigns.nvim", -- git highlighter, blame, etc
     opts = {
       signs = {
-        add = { text = icon("ui.VLineLeft") },
-        change = { text = icon("ui.VLineLeft") },
-        changedelete = { text = icon("ui.VLineLeft") },
-        delete = { text = icon("ui.ArrowRight") },
-        topdelete = { text = icon("ui.ArrowRight") },
-        untracked = { text = icon("ui.VDashLineLeft") },
+        add = { text = i("ui.VLineLeft") },
+        change = { text = i("ui.VLineLeft") },
+        changedelete = { text = i("ui.VLineLeft") },
+        delete = { text = i("ui.ArrowRight") },
+        topdelete = { text = i("ui.ArrowRight") },
+        untracked = { text = i("ui.VDashLineLeft") },
       },
       preview_config = { border = Cange.get_config("ui.border") },
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 800,
-      },
-      current_line_blame_formatter = " " .. icon("git.Commit") .. " <author>, <author_time:%d.%m.%y> " .. icon("ui.Note") .. " <summary>",
+      current_line_blame = true,
+      current_line_blame_formatter = line_format,
     },
   },
 }
--- stylua: ignore start
