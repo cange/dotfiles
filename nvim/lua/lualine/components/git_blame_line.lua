3a02c242922a54eb41eab7b6a0c70716d5c2fa7d
Christian Angermann <christian.angermann@visable.com>
2023-05-15 00:44:38 +0200

    refactor(nvim): simplify git blame line msg length

--- nvim/lua/lualine/components/git_blame_line.lua
+++ nvim/lua/lualine/components/git_blame_line.lua
@@ -6,61 +6,21 @@ end
 
 local M = require("lualine.component"):extend()
 
----@alias GitBlameLineConfig {icon: string,color: string, ignore_filetypes: string[]}
+---@alias GitBlameLineConfig {icon: string,color: string}
 ---@alias GitBlameLineCache {win_count: integer, total_width: integer}
 --
 ---@type GitBlameLineConfig
 local config = {
-  ---@diagnostic disable: assign-type-mismatch
   icon = "",
+  ---@diagnostic disable-next-line: assign-type-mismatch
   color = Cange.get_hl_hex("lualine_c_inactive", "fg"),
-  ---@diagnostic enable: assign-type-mismatch
-  ignore_filetypes = {
-    "telescopeprompt",
-    "telescoperesults",
-    "telescopepreviewer",
-    "cmp_menu",
-    "notify",
-    "fidget",
-    "whichkey",
-    "harpoon",
-  },
 }
-
----@type GitBlameLineCache
-local cache = {
-  win_count = 0,
-  total_width = 0,
-}
-
----Determine total width of all windows
----@return integer|string[]
-local function get_total_width()
-  local wins = vim.api.nvim_list_wins()
-  if cache.win_count == #wins then return cache.total_width end
-
-  local total_width = 0
-  for _, winid in ipairs(wins) do
-    local bufnr = vim.api.nvim_win_get_buf(winid)
-    local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-
-    if not vim.tbl_contains(config.ignore_filetypes, filetype:lower()) then
-      local width = vim.api.nvim_win_get_width(winid)
-      total_width = total_width + width
-    end
-  end
-
-  cache.total_width = total_width
-  cache.win_count = #wins
-
-  return total_width
-end
 
 ---@param msg string
 ---@return string
 local function shorten_by_win_width(msg)
   local len = 72
-  local w = get_total_width()
+  local w = vim.o.columns
 
   if w < 156 and w > 128 then
     len = len / 2
@@ -71,7 +31,8 @@ local function shorten_by_win_width(msg)
   end
 
   local suffix = len > 0 and #msg > len and "…" or ""
-  return msg:sub(1, len - #suffix) .. suffix
+
+  return vim.trim(msg):sub(1, len - #suffix) .. suffix
 end
 
 function M.init(self, opts)
@@ -79,8 +40,9 @@ function M.init(self, opts)
   M.super.init(self, vim.tbl_extend("force", opts, { color = config.color }))
 end
 function M.update_status()
-  local msg = vim.b[vim.api.nvim_get_current_buf()].gitsigns_blame_line or ""
-  return shorten_by_win_width(vim.trim(msg))
+  local msg = vim.b.gitsigns_blame_line or ""
+
+  return shorten_by_win_width(msg)
 end
 
 return M
