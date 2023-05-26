ec4aed60ede2d212e2f4b75c0cbbe762fa515899
Christian Angermann <28717+cange@users.noreply.github.com>
2023-05-26 21:46:07 +0200

    feat(nvim): resolve health check deprecations

--- nvim/lua/cange/health.lua
+++ nvim/lua/cange/health.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 function M.check()
-  vim.health.report_start("Cange")
+  vim.health.start("Cange")
 
   for _, cmd in ipairs({ "git", "rg", "deno" }) do
     if vim.fn.executable(cmd) == 1 then
-      vim.health.report_ok(("`%s` is installed"):format(cmd))
+      vim.health.ok(("`%s` is installed"):format(cmd))
     else
-      vim.health.report_warn(("`%s` is not installed"):format(cmd))
+      vim.health.warn(("`%s` is not installed"):format(cmd))
     end
   end
 end
