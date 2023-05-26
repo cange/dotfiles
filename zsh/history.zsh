a638e4291dd3838feeade989ac0f80f4377acaab
Christian Angermann <28717+cange@users.noreply.github.com>
2023-05-26 22:35:39 +0200

    feat(zsh): handle history behaviour

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,10 +1,15 @@
 ##############################################################################
 # History Configuration
+# https://www.soberkoder.com/better-zsh-history/
 ##############################################################################
-HISTSIZE=5000               # How many lines of history to keep in memory
-HISTFILE=~/.zsh_history     # Where to save history to disk
-SAVEHIST=5000               # Number of history entries to save to disk
 HISTDUP=erase               # Erase duplicates in the history file
-setopt appendhistory        # Append history to the history file (no overwriting)
-setopt sharehistory         # Share history across terminals
-setopt incappendhistory     # Immediately append to the history file, not just when a term is killed
+HISTFILE=~/.zsh_history     # path/location of the history file
+HISTSIZE=100000             # number of commands that are loaded into memory
+SAVEHIST=100000             # number of commands that are stored
+
+setopt APPEND_HISTORY       # Append history to the history file (no overwriting)
+setopt HIST_FIND_NO_DUPS    # Do not display duplicates in the history list
+setopt INC_APPEND_HISTORY   # Immediately append to the history file, not just when a term is killed
+setopt SHARE_HISTORY        # Share history across terminals
+
+export HISTTIMEFORMAT="[%F %T] " #
