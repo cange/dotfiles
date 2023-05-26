f9035772414052ef8298f506212630de171da721
Christian Angermann <28717+cange@users.noreply.github.com>
2023-05-26 23:08:59 +0200

    feat(zsh): fuzzy search CLI

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,23 +1,27 @@
+source "$ZDOTDIR/helpers.zsh"
+
 # see https://github.com/mrnugget/dotfiles/blob/master/zshrc#L496
-set_fzf_default_opts() {
-  if [[ $KITTY_COLORS == "light" ]]; then
-    export FZF_DEFAULT_OPTS=''
-    # --color=bg+:#DEECF9,bg:#FFFFFF,spinner:#3f5fff,hl:#586e75
-    # --color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#3f5fff
-    # --color=marker:#3f5fff,fg+:#839496,prompt:#3f5fff,hl+:#3f5fff'
+
+source_if_exists "$(brew --prefix)/opt/fzf/shell/completion.zsh"
+source_if_exists "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+
+if type fzf &> /dev/null; then
+  # Theme: Nightfox/Style: terafox
+  # https://github.com/EdenEast/nightfox.nvim/tree/main/extra/terafox
+  export FZF_DEFAULT_OPTS="--no-height --color=bg+:#293e40,gutter:-1,pointer:#e6eaea,info:#7aa4a1,hl:#ff8349,hl+:#ff8349"
+
+  if type fd &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type f --color=never --hidden'
+    export FZF_ALT_C_COMMAND='fd --type d . --color=never --hidden --exclude=.git'
+    export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --exclude=.git"
+
+    if type bat &> /dev/null; then
+      export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :50 {}'"
+    else
+      echo "bat is not installed, skipping FZF_CTRL_T_OPTS"
+    fi
   else
-    export FZF_DEFAULT_OPTS=''
+    echo "fd is not installed, skipping FZF_DEFAULT_COMMAND"
   fi
-}
-
-if [[ -e /usr/local/opt/fzf/shell/completion.zsh ]]; then
-  source /usr/local/opt/fzf/shell/key-bindings.zsh
-  source /usr/local/opt/fzf/shell/completion.zsh
-fi
-
-if type fzf &> /dev/null && type rg &> /dev/null; then
-  export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!vendor/*"'
-  export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!vendor/*"'
-  # export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-  set_fzf_default_opts
 fi
