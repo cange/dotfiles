7613958fce9d690b94b5391f7d2be918b7a0d852
Christian Angermann <28717+cange@users.noreply.github.com>
2023-06-02 09:42:34 +0200

    chore(zsh): remove obsolete aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,13 +2,17 @@ setopt pushdminus
 
 # --- tools
 alias vim='nvim'
-alias gui='open -a fork .' # allow to run executables within fork
+alias gui='open -a fork ' # allow to run executables within fork
 # tools ---
 #
 # --- confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
+
+# --- handy helpers
+alias q='exit'
+alias k='clear'
 
 # --- grep output
 alias grep='grep --color=auto'
@@ -38,30 +42,12 @@ alias h='history'
 alias hg='history | grep'
 # search history ---
 #
-# --- work docker
-alias dc='time wlw-dc'
-alias dce='time wlw-exec'
-alias dcy='time wlw-exec yarn'
-alias dcb='time wlw-exec bundle'
-alias dcrs='time wlw-restart'
-alias dcrb='time wlw-rebuild'
-alias q='exit'
-alias k='clear'
-# work docker ---
-#
 # ruby bundle
 alias be='bundle exec'
 alias bi='bundle install'
 alias server="sudo python3 -m http.server 80"
 
 # --- work
-alias cr='cd ~/workspace/wlw/customer-report-frontend'
-alias lyc='cd ~/workspace/wlw/lyc'
-alias sf='cd ~/workspace/wlw/supplier-frontend'
-alias sff='cd ~/workspace/wlw/supplier-facts-frontend'
-alias sg='cd ~/workspace/wlw/wlw_styleguide'
-alias statistics='cd ~/workspace/wlw/statistics'
-alias user='cd ~/workspace/wlw/user'
 alias work='cd ~/workspace'
 # work ---
 #
