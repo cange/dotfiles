1885800c40f49007c53aa5be6097d697e4125970
Christian Angermann <28717+cange@users.noreply.github.com>
2023-07-03 00:44:24 +0200

    feat(nvim): update flash search
    Match beginning of words only

--- nvim/lua/cange/plugins/support.lua
+++ nvim/lua/cange/plugins/support.lua
@@ -136,6 +136,36 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { mode = function(str) return "\\<" .. str end },
+            jump = { autojump = true },
+          })
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote({
+            search = { mode = function(str) return "\\<" .. str end },
+            jump = { autojump = true },
+          })
+        end,
+        desc = "Remote Flash",
+      },
+    },
   },
 
   { -- smooth scrolling
