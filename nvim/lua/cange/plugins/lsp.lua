d11b1c2c1340a6d6c6de95c71c4b5be416f6efb1
Christian Angermann <28717+cange@users.noreply.github.com>
2023-08-20 22:10:48 +0200

    refactor(nvim): move config sources to the locations where they are used

--- nvim/lua/cange/plugins/lsp.lua
+++ nvim/lua/cange/plugins/lsp.lua
@@ -1,3 +1,16 @@
+local i = Cange.get_icon
+local server_sources = {
+  "bashls", -- bash
+  "cssls", -- css
+  "html", -- html
+  "jsonls", -- json
+  "ruby_ls", -- ruby
+  "svelte", -- svelte
+  "tsserver", -- javascript, typescript, etc.
+  "volar", -- vue 3 and 2
+  "yamlls", -- yaml
+}
+
 return {
   { -- lspconfig
     "neovim/nvim-lspconfig", -- configure LSP servers
@@ -12,9 +25,9 @@ return {
         ui = {
           border = Cange.get_config("ui.border"),
           icons = {
-            package_installed = Cange.get_icon("ui.Check"),
-            package_pending = Cange.get_icon("ui.Sync"),
-            package_uninstalled = Cange.get_icon("ui.Close"),
+            package_installed = i("ui.Check"),
+            package_pending = i("ui.Sync"),
+            package_uninstalled = i("ui.Close"),
           },
         },
         log_level = vim.log.levels.INFO,
@@ -22,61 +35,10 @@ return {
       })
       require("mason-lspconfig").setup({
         automatic_installation = true,
-        ensure_installed = Cange.get_config("lsp.server_sources"),
+        ensure_installed = server_sources,
       })
       local handler = require("cange.lsp").setup_handler
       require("mason-lspconfig").setup_handlers({ handler })
-    end,
-  },
-
-  { -- formatters
-    "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "jayp0521/mason-null-ls.nvim", -- bridges mason.nvim with the null-ls plugin
-      "jose-elias-alvarez/null-ls.nvim", -- syntax formatting, diagnostics (dependencies npm pacakges)
-      "jose-elias-alvarez/typescript.nvim", -- enables LSP features for TS/JS
-      "williamboman/mason.nvim",
-    },
-    config = function()
-      -- https://github.com/jayp0521/mason-null-ls.nvim#default-configuration
-      local sources = Cange.get_config("lsp.null_ls_sources")
-      require("mason-null-ls").setup({
-        automatic_installation = true,
-        ensure_installed = sources,
-      })
-
-      -- Install all necessary packages at once
-      vim.api.nvim_create_user_command(
-        "MasonInstallAll",
-        function() vim.cmd("MasonInstall " .. table.concat(sources, " ")) end,
-        {}
-      )
-
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/
-      local nls = require("null-ls")
-      local diagnostics = nls.builtins.diagnostics
-      local code_actions = nls.builtins.code_actions
-      local formatting = nls.builtins.formatting
-      code_actions.typescript = require("typescript.extensions.null-ls.code-actions")
-
-      nls.setup({
-        update_in_insert = false, -- if false, diagnostics will run upon exiting insert mode
-        sources = {
-          code_actions.typescript,
-
-          diagnostics.jsonlint,
-          diagnostics.rubocop, -- ruby
-          diagnostics.yamllint,
-          diagnostics.zsh,
-
-          formatting.prettierd, -- js, css, html, json, etc
-          formatting.rubocop, -- ruby
-          formatting.beautysh.with({ extra_args = { "--indent-size", "2" } }), -- bash, zsh
-          formatting.stylelint, -- css
-          formatting.stylua, -- lua
-        },
-      })
     end,
   },
 
