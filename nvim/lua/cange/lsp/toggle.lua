c9b526af8e367157057c117668645c53dbed01b0
Christian Angermann <28717+cange@users.noreply.github.com>
2023-09-17 13:34:21 +0200

    feat(nvim): establish dedicated toggle helper

---
+++ nvim/lua/cange/lsp/toggle.lua
@@ -0,0 +1,28 @@
+local format_on_save = Cange.get_config("lsp.format_on_save")
+local diagnostic_virtual_text = Cange.get_config("lsp.diagnostic_virtual_text")
+
+---Toggles given state, returns it and logs the change.
+---@param state boolean
+---@param title string
+---@return boolean
+local function toggle(state, title)
+  state = not state
+  local msg = state and "enabled" or "disabled"
+  Log:info(msg, title)
+  return state
+end
+
+local M = {}
+
+function M.virtual_text()
+  local toggle_state = toggle(diagnostic_virtual_text, "Diagnostic virtual inline text")
+  Cange.set_config("lsp.diagnostic_virtual_text", toggle_state)
+  vim.diagnostic.config({ virtual_text = toggle_state })
+end
+
+function M.format_on_save()
+  local toggle_state = toggle(format_on_save, "Auto format on save")
+  Cange.set_config("lsp.format_on_save", toggle_state)
+end
+
+return M
