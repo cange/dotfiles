8b61dcc1448934d00a2624bfa6e6b5a144c82f99
Christian Angermann <28717+cange@users.noreply.github.com>
2023-09-25 23:06:16 +0200

    pref(nvim): use NvChad inspired LSP status

--- nvim/lua/lualine/components/lsp_status.lua
+++ nvim/lua/lualine/components/lsp_status.lua
@@ -1,31 +1,42 @@
 local M = require("lualine.component"):extend()
-local active = Cange.get_icon("ui.Eye") .. " LSP"
-local inactive = Cange.get_icon("ui.EyeClosed") .. " LSP"
-local blacklist = { "null-ls", "copilot" }
-local label = inactive
-local state = inactive
-local old_ft = nil
+local refresh_count = 0
+local cache = {}
+local debug = false
+local i = Cange.get_icon
+local icons = {
+  ["active"] = i("ui.Eye"),
+  ["inactive"] = i("ui.EyeClosed"),
+}
+
+local function get_active_clients()
+  local active_clients = {}
+  local buf = vim.api.nvim_get_current_buf()
+  vim.lsp.for_each_buffer_client(buf, function(client)
+    local blacklisted = vim.tbl_contains({ "null-ls", "copilot" }, client.name)
+    if client.attached_buffers[buf] and not blacklisted then table.insert(active_clients, client.name) end
+    refresh_count = refresh_count + 1
+  end)
+  return active_clients
+end
+
+---@param clients table
+---@param debug_label string
+---@return string
+local function content(clients, debug_label)
+  local state = clients ~= nil and #clients == 0 and "inactive" or "active"
+  local output = vim.o.columns > 100 and #clients > 0 and ": " .. table.concat(clients, ", ") or ""
+  output = icons[state] .. " LSP" .. output
+  if debug then vim.print("lsp " .. debug_label .. " -c: " .. refresh_count .. " -output: " .. output) end
+  return output
+end
 
 function M:update_status()
-  if not rawget(vim, "lsp") then return inactive end
-
-  local new_ft = vim.fn.expand("%:e")
-  -- PERF: avoid redundant detection when same filetype and is already active
-  if old_ft == new_ft and state ~= inactive then return label end
-  old_ft = new_ft
-
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
-    local blacklisted = vim.tbl_contains(blacklist, client.name)
-    if client.attached_buffers[vim.api.nvim_get_current_buf()] and not blacklisted then
-      label = (vim.o.columns > 100 and "%#St_LspStatus#" .. active .. ": " .. client.name) or active
-      state = active
-      goto continue
-    else
-      label = inactive
-    end
-  end
-  ::continue::
-  return label
+  if not vim.lsp then return content({}, "lsp not supported") end
+  local ft = vim.fn.expand("%:e")
+  if cache[ft] then return content(cache[ft], "cache") end
+  local active_clients = get_active_clients()
+  if #active_clients > 0 then cache[ft] = active_clients end
+  return content(cache[ft] or {}, "fallback")
 end
 
 return M
