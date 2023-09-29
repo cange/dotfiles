1979511d2d1978ab3ff29874c7e0586a28fd61a4
Christian Angermann <28717+cange@users.noreply.github.com>
2023-09-29 10:49:35 +0200

    chore(nvim): update keymaps of flash.nvim

--- nvim/lua/cange/plugins/support.lua
+++ nvim/lua/cange/plugins/support.lua
@@ -144,35 +144,10 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
+    -- stylua: ignore
     keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump({
-            search = { mode = function(str) return "\\<" .. str end },
-            jump = { autojump = true },
-          })
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote({
-            search = { mode = function(str) return "\\<" .. str end },
-            jump = { autojump = true },
-          })
-        end,
-        desc = "Remote Flash",
-      },
+      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
 
