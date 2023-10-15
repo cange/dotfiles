ad8faeb2a24d613f4608f19f9e9dd59b35787b6b
Christian Angermann <28717+cange@users.noreply.github.com>
2023-10-15 15:16:23 +0200

    refactor(nvim): split formatting, linting

---
+++ nvim/lua/cange/plugins/formatting.lua
@@ -0,0 +1,27 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    formatters_by_ft = {
+      json = { "prettierd" },
+      jsonc = { "prettierd" },
+      lua = { "stylua" },
+      html = { "prettierd" },
+      markdown = { 'prettierd' },
+      yaml = { "prettierd" },
+      ruby = { "rubocop" },
+      -- css
+      css = { { "prettierd", "stylelint" } },
+      scss = { { "prettierd", "stylelint" } },
+      -- js
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      svelte = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      vue = { "prettierd" },
+      ["_"] = { "trim_whitespace" },
+    },
+  },
+}
--- nvim/lua/cange/plugins/lsp.lua
+++ nvim/lua/cange/plugins/lsp.lua
@@ -45,7 +45,10 @@ return {
 
   { -- loads LSP formatter and linter
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = "williamboman/mason.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+    },
     opts = {
       ensure_installed = {
         "eslint_d",
@@ -59,68 +62,6 @@ return {
       },
       auto_update = true,
     },
-  },
-
-  { -- formatting
-    "stevearc/conform.nvim",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      formatters_by_ft = {
-        json = { "prettierd" },
-        jsonc = { "prettierd" },
-        lua = { "stylua" },
-        html = { "prettierd" },
-        markdown = { "prettierd" },
-        yaml = { "prettierd" },
-        ruby = { "rubocop" },
-        -- css
-        css = { { "prettierd", "stylelint" } },
-        scss = { { "prettierd", "stylelint" } },
-        -- js
-        javascript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        svelte = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        vue = { "prettierd" },
-        ["_"] = { "trim_whitespace" },
-      },
-    },
-  },
-
-  { -- linting
-    "mfussenegger/nvim-lint",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile", "InsertLeave" },
-    opts = {
-      json = { "jsonlint" },
-      markdown = { "markdownlint" },
-      ruby = { "rubocop" },
-      yaml = { "yamllint" },
-      -- css
-      css = { "stylelint" },
-      scss = { "stylelint" },
-      -- js
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      vue = { "eslint_d" },
-    },
-    keys = {
-      { "<leader>l", "<cmd>lua R('lint').try_lint()<CR>", desc = "Trigger linting for current file" },
-    },
-    config = function(_, opts)
-      local lint = require("lint")
-      lint.linters_by_ft = opts
-
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = vim.api.nvim_create_augroup("lint", { clear = true }),
-        callback = function() lint.try_lint() end,
-      })
-    end,
   },
 
   { -- slim language support (Ruby)
