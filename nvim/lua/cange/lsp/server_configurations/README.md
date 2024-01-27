f939b90f05fee84feccf269236001efa4a191e14
Christian Angermann <28717+cange@users.noreply.github.com>
2024-01-27 17:46:45 +0100

    refactor(nvim): separate linting/formatting and lsp

--- nvim/lua/cange/lsp/server_configurations/README.md
+++
@@ -1,8 +0,0 @@
-# LSP Settings
-
-Contains LSP settings for each individual language/scope.
-
-The file name in here must been match with list of [server_configurations.md] resp.
-`help lspconfig-all`. e.g. server "sumneko_lua" = `sumneko_lua.lua`
-
-[server_configurations.md]: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
--- nvim/lua/cange/plugins/lsp.lua
+++ nvim/lua/cange/plugins/lsp.lua
@@ -1,129 +1,6 @@
 local i = Cange.get_icon
-local servers = {
-  "bashls",
-  "cssls",
-  "eslint",
-  "html",
-  "jsonls",
-  "lemminx", -- xml, xsd, xsl, xslt, svg
-  "lua_ls",
-  "ruby_ls",
-  "svelte",
-  "tsserver", -- javascript, typescript, etc.
-  "volar", -- vue 3 and 2
-  "yamlls",
-}
-local formatters_and_linters = {
-  "eslint_d",
-  "jsonlint",
-  "markdownlint",
-  "prettierd",
-  "rubocop",
-  "stylelint",
-  "stylua",
-  "xmlformatter", -- svg
-  "yamllint",
-}
 
 return {
-  { -- auto install main LSPs
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      { -- json/yaml schema support
-        "b0o/SchemaStore.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-      },
-    },
-    opts = {
-      ensure_installed = servers,
-      automatic_installation = true,
-    },
-  },
-
-  { -- loads LSP formatter and linter
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-    },
-    opts = {
-      ensure_installed = formatters_and_linters,
-      auto_update = true,
-    },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    lazy = true,
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local config = require("cange.lsp").server_config
-      for _, server in ipairs(servers) do
-        local ok, server_config = pcall(require, "cange.lsp.server_configurations." .. server)
-        if ok then config = vim.tbl_deep_extend("force", config, server_config) end
-
-        require("lspconfig")[server].setup(config)
-      end
-      require("cange.lsp").update_diagnostics()
-
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = Cange.get_config("ui.border"),
-        title = "Hover",
-      })
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = Cange.get_config("ui.border"),
-        title = "Signature Help",
-      })
-    end,
-    -- stylua: ignore
-    keys = require("cange.lsp").keymaps,
-  },
-
-  { -- A pretty list for showing diagnostics
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-    keys = function()
-      local opts = { skip_groups = true, jump = true }
-      local trouble = require("trouble")
-
-      return {
-        { "<leader>tx", "<cmd>TroubleToggle<CR>", desc = "All Diagnostics" },
-        { "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics" },
-        { "<leader>td", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics" },
-        { "<leader>tq", "<cmd>TroubleToggle quickfix<CR>", desc = "Quickfix List" },
-        { "<leader>tl", "<cmd>TroubleToggle loclist<CR>", desc = "Location List" },
-        { "gR", "<cmd>TroubleToggle lsp_references<CR>", desc = "LSP Refenrences Search" },
-        {
-          "[t",
-          function()
-            if trouble.is_open() then
-              trouble.previous(opts)
-            else
-              vim.diagnostic.goto_prev()
-            end
-          end,
-          desc = "Prev Diagnostic",
-        },
-
-        {
-          "]t",
-          function()
-            if trouble.is_open() then
-              trouble.next(opts)
-            else
-              vim.diagnostic.goto_next()
-            end
-          end,
-          desc = "Next Diagnostic",
-        },
-      }
-    end,
-  },
-
   { -- managing & installing LSP servers, linters & formatters
     "williamboman/mason.nvim",
     dependencies = "neovim/nvim-lspconfig",
@@ -142,6 +19,138 @@ return {
     keys = { { "<leader>e2", "<cmd>Mason<CR>", desc = "Mason info" } },
   },
 
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+      "williamboman/mason-lspconfig.nvim", -- auto install main LSPs
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local util = require("lspconfig.util")
+      local function get_typescript_server_path(root_dir)
+        local global_ts = vim.fn.expand("$HOME")
+          .. "/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib"
+        local found_ts = ""
+        local function check_dir(path)
+          found_ts = path .. "/node_modules/typescript/lib"
+          if util.path.exists(found_ts) then return path end
+        end
+        if util.search_ancestors(root_dir, check_dir) then
+          return found_ts
+        else
+          return global_ts
+        end
+      end
+
+      local server_configs = {
+        bashls = {},
+        cssls = {},
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectories = { mode = "auto" },
+          },
+        },
+        html = {},
+        lemminx = {}, -- xml, xsd, xsl, xslt, svg
+        ruby_ls = {},
+        svelte = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              workspace = {
+                library = {
+                  -- Make the server aware of Neovim runtime files
+                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                  [vim.fn.stdpath("config") .. "/lua"] = true,
+                },
+              },
+              telemetry = { enable = false },
+            },
+          },
+        },
+        tsserver = { -- javascript, typescript, etc.
+          init_options = {
+            preferences = {
+              disableSuggestions = true,
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+        volar = { -- vue 3 and 2
+          -- enable typescript Take Over Mode
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar
+          filetypes = { "vue" },
+          on_new_config = function(new_config, new_root_dir)
+            new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+          end,
+        },
+        jsonls = {
+          settings = {
+            json = { schemas = require("schemastore").json.schemas() },
+            validate = { enable = true },
+          },
+        },
+        yamlls = {
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              schemaStore = {
+                enable = true,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+              schemas = require("schemastore").yaml.schemas({
+                extra = {
+                  {
+                    description = "Definition of Visables pattern lib documentation data",
+                    fileMatch = {
+                      "**/wlw_styleguide/**/documentation.yml",
+                      "**/wlw_styleguide/**/documentation.yaml",
+                    },
+                    name = "wlw styleguide",
+                    url = vim.fn.expand("file://$HOME/workspace/services/wlw_styleguide/schemas/docs.schema.json"),
+                  },
+                },
+              }),
+            },
+            validate = { enable = true },
+          },
+        },
+      }
+      local default_config = require("cange.lsp").server_config
+
+      for server, server_config in pairs(server_configs) do
+        local config = vim.tbl_deep_extend("force", default_config, server_config)
+
+        require("lspconfig")[server].setup(config)
+      end
+
+      require("cange.lsp").update_diagnostics()
+      require("mason-lspconfig").setup({
+        ensure_installed = vim.tbl_keys(server_configs),
+        automatic_installation = true,
+      })
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = Cange.get_config("ui.border"),
+        title = "Hover",
+      })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = Cange.get_config("ui.border"),
+        title = "Signature Help",
+      })
+    end,
+    -- stylua: ignore
+    keys = require("cange.lsp").keymaps,
+  },
+
   { -- Extensible UI notifications and LSP progress messages.
     "j-hui/fidget.nvim",
     opts = {
@@ -151,54 +160,5 @@ return {
         },
       },
     },
-  },
-
-  {
-    "cange/specto.nvim",
-    lazy = false,
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    opts = {},
-    keys = {
-      { "<LocalLeader>o", "<cmd>Specto toggle only<CR>", desc = "Toggle Only test block" },
-      { "<LocalLeader>s", "<cmd>Specto toggle skip<CR>", desc = "Toggle Skip test block" },
-    },
-  },
-
-  { -- A pretty list for showing diagnostics
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-    keys = function()
-      local opts = { skip_groups = true, jump = true }
-      local trouble = require("trouble")
-
-      return {
-        { "<leader>tt", "<cmd>TroubleToggle<CR>", desc = "All Diagnostics" },
-        { "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics" },
-        { "<leader>td", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics" },
-        { "<leader>tq", "<cmd>TroubleToggle quickfix<CR>", desc = "Quickfix List" },
-        { "<leader>tl", "<cmd>TroubleToggle loclist<CR>", desc = "Location List" },
-        { "gR", "<cmd>TroubleToggle lsp_references<CR>", desc = "LSP Refenrences Search" },
-        {
-          "[q",
-          function()
-            trouble.open()
-            trouble.previous(opts)
-          end,
-          desc = "Prev Diagnostic",
-        },
-
-        {
-          "]q",
-          function()
-            trouble.open()
-            trouble.next(opts)
-          end,
-          desc = "Next Diagnostic",
-        },
-      }
-    end,
   },
 }
