d6b25ac13261aae3797562ca1f560541a6b6fe35
Christian Angermann <28717+cange@users.noreply.github.com>
2024-01-31 22:09:03 +0100

    feat(zsh): reduce log noise while traveling through dirs

--- zsh/aliases.docker.zsh
+++ zsh/aliases.docker.zsh
@@ -1,97 +1,108 @@
 #!/bin/zsh
 
 export CANGE_OLD_PWD=$PWD
-local _cange_cached_compose_file=$COMPOSE_FILE
+local _cached_compose_file=$COMPOSE_FILE
 
-# Execute `docker compose` from within a services directory and the the dir name as project name.
-function _cange_docker_compose() {
+# File detection mechanism to automatically detect the appropriate Docker
+function _docker_compose_init_detection() {
+	local dir_state=""
+
+	function log() {
+		local title="$(_chalk "blue" "󰡨") docker compose:"
+		printf "%s %s %s\n" "$title" "$1" "$dir_state"
+		dir_state=""
+	}
+
+	function file_level_dir_depth() {
+		local file_to_find="$1"
+		local current_dir="$PWD"
+		local level_count=1
+
+		while [[ "$current_dir" != "/" ]]; do
+			level_count=$(($level_count + 1))
+			if [[ -e "$current_dir/$file_to_find" ]]; then
+				return 0
+			fi
+			current_dir=$(dirname "$current_dir")
+		done
+		echo $level_count
+		level_count=0
+		return 1
+	}
+
+	function _docker_compose_detect_file() {
+		# continue when docker project
+		if [[ ! -e "Dockerfile" ]]; then return 0; fi
+
+		local files=("docker-compose.yml" "docker-compose.yaml" "compose.yml" "compose.yaml")
+		local count=$1
+		local file="${files[$count]}"
+
+		if [[ $count -gt ${#files[@]} && -z $COMPOSE_FILE ]]; then
+			export COMPOSE_FILE=$_cached_compose_file
+			log "No local file detected! Reset to default $(_chalk "bold" "\$COMPOSE_FILE")"
+			return 0
+		fi
+
+		local dir_level=$(printf "%d" "$(file_level_dir_depth "$file")")
+
+		if [[ ($dir_level -eq 0 && -e $file) ]]; then
+			_cached_compose_file=$([[ -z $COMPOSE_FILE ]] && print $_cached_compose_file || print $COMPOSE_FILE)
+			unset COMPOSE_PROJECT_NAME
+			unset COMPOSE_FILE
+			log "File detected! Unset $(_chalk "bold" "\$COMPOSE_FILE, \$COMPOSE_PROJECT_NAME")"
+			return 0
+		elif [[ $dir_level -eq 0 && $file != $COMPOSE_FILE ]]; then
+			return 0
+		else
+			_docker_compose_detect_file $(($count + 1))
+			return 0
+		fi
+	}
+
+	# initialise detection
+	_docker_compose_detect_file 1
+
+	# Hook to triggered when the current directory changes
+	function _docker_compose_chpwd_hook() {
+		if [[ -z $CANGE_OLD_PWD ]]; then CANGE_OLD_PWD=$PWD; fi
+
+		if [[ $PWD != $CANGE_OLD_PWD ]]; then
+			CANGE_OLD_PWD=$PWD
+			dir_state="$(_chalk "yellow" "󰛨") Dir changed!"
+			_docker_compose_detect_file 1
+		fi
+	}
+
+	# Enable chpwd hook
+	add-zsh-hook chpwd _docker_compose_chpwd_hook
+}
+
+_docker_compose_init_detection
+
+# Execute `docker compose` from within a services directory and the dir name as
+# project name.
+function _docker_compose() {
 	local cmd=$1
 	local dirname=$([[ -z $COMPOSE_PROJECT_NAME ]] && print "" || print $(basename $(pwd)))
 	shift 1
 	docker-compose "$cmd" $dirname "$@"
 }
 
-function _cange_docker_compose_init() {
-	local file_match=''
-	local dir_state=""
-
-	function log() {
-		local title="$(_cange_chalk "blue" "󰡨") docker compose:"
-		printf "%s %s %s\n" "$title" "$1" "$dir_state"
-	}
-
-	function find_file_upwards() {
-		local file_to_find="$1"
-		local current_dir="$PWD"
-
-		while [[ "$current_dir" != "/" ]]; do
-			if [[ -e "$current_dir/$file_to_find" ]]; then
-				file_match=$file_to_find
-				return 0 # Success, file found
-			fi
-			current_dir=$(dirname "$current_dir")
-		done
-		return 1 # Failure, file not found
-	}
-
-	function _cange_docker_compose_detect_file() {
-		local files=("docker-compose.yml" "docker-compose.yaml" "compose.yml" "compose.yaml")
-		local count=$1
-		local file="${files[$count]}"
-
-		if [[ $count -gt ${#files[@]} ]]; then
-			export COMPOSE_FILE=$_cange_cached_compose_file
-			log "No local file detected! Restore defaults $(_cange_chalk "bold" "\$COMPOSE_FILE, \$COMPOSE_PROJECT_NAME")"
-			return 0
-		fi
-
-		find_file_upwards "$file"
-
-		if [[ (${#file_match} -gt 0 && $file != $COMPOSE_FILE) ]]; then
-			_cange_cached_compose_file=$([[ -z $COMPOSE_FILE ]] && print $_cange_cached_compose_file || print $COMPOSE_FILE)
-			unset COMPOSE_PROJECT_NAME
-			unset COMPOSE_FILE
-			log "File detected! Unset $(_cange_chalk "bold" "\$COMPOSE_FILE, \$COMPOSE_PROJECT_NAME")"
-			return 0
-		else _cange_docker_compose_detect_file $(($count + 1)); fi
-
-		return 1 # no file found
-	}
-
-	# initialise detection
-	_cange_docker_compose_detect_file 1
-
-	function _cange_docker_compose_chpwd() {
-		if [[ -z $CANGE_OLD_PWD ]]; then CANGE_OLD_PWD=$PWD; fi
-
-		if [[ $PWD != $CANGE_OLD_PWD ]]; then
-			CANGE_OLD_PWD=$PWD
-			dir_state="$(_cange_chalk "yellow" "󰛨") Directory changed!"
-			_cange_docker_compose_detect_file 1
-		fi
-	}
-
-	# Enable the chpwd hook
-	add-zsh-hook chpwd _cange_docker_compose_chpwd
-	dir_state=""
-}
-
-_cange_docker_compose_init
-
 # aliases
 #
 # --- docker compose
 # see https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/docker-compose
-alias dco="_cange_docker_compose "                        # Docker-compose main command
-alias dcbl="_cange_docker_compose exec bundle"            # Execute Rails bundle command
-alias dcdn="_cange_docker_compose down"                   # Stop and remove container
-alias dce="_cange_docker_compose exec"                    # Execute command inside a container
-alias dcl="_cange_docker_compose logs --follow --tail=10" # Show container logs
-alias dcrb="dcup --build --force-recreate"                # Rebuild
-alias dcrbl="dcrb && dcl"                                 # Rebuild with followup log
-alias dcrs="_cange_docker_compose restart && dcl"         # Restart container and show logs
-alias dcup="_cange_docker_compose up --detach"            # Start container and its dependencies
-alias dcy="_cange_docker_compose exec yarn"               # Execute yarn command
+alias dco="_docker_compose "                        # Docker-compose main command
+alias dcbl="_docker_compose exec bundle"            # Execute Rails bundle command
+alias dcdn="_docker_compose down"                   # Stop and remove container
+alias dce="_docker_compose exec"                    # Execute command inside a container
+alias dcl="_docker_compose logs --follow --tail=10" # Show container logs
+alias dcrb="dcup --build --force-recreate"          # Rebuild
+alias dcrbl="dcrb && dcl"                           # Rebuild with followup log
+alias dcrs="_docker_compose restart && dcl"         # Restart container and show logs
+alias dcup="_docker_compose up --detach"            # Start container and its dependencies
+alias dcy="_docker_compose exec yarn"               # Execute yarn command
 # docker compose ---
 
 # --- docker
--- zsh/helpers.zsh
+++ zsh/helpers.zsh
@@ -1,35 +1,35 @@
 # --- helpers
-function _cange_source_if_exists() {
-  [[ -f "$1" ]] && source "$1"
+function _source_if_exists() {
+	[[ -f "$1" ]] && source "$1"
 }
 
-function _cange_add_plugin() {
-  local name=$(echo $1 | cut -d "/" -f 2)
-  local target="$Z_CONFIG_DIR/plugins/$name"
-  if [[ -d $target ]]; then
-    _cange_source_if_exists "$target/$name.plugin.zsh" || \
-    _cange_source_if_exists "$target/$name.zsh-theme" || \
-    _cange_source_if_exists "$target/$name.zsh"
-  else
-    git clone "https://github.com/$1.git" "$target"
-  fi
+function _add_plugin() {
+	local name=$(echo $1 | cut -d "/" -f 2)
+	local target="$Z_CONFIG_DIR/plugins/$name"
+	if [[ -d $target ]]; then
+		_source_if_exists "$target/$name.plugin.zsh" ||
+			_source_if_exists "$target/$name.zsh-theme" ||
+			_source_if_exists "$target/$name.zsh"
+	else
+		git clone "https://github.com/$1.git" "$target"
+	fi
 }
 
-# Example: "$(_cange_chalk "blue" "msg")"
-function _cange_chalk() {
-  local color=$1
-  local text=$2
+# Example: "$(_chalk "blue" "msg")"
+function _chalk() {
+	local color=$1
+	local text=$2
 
-  case $color in
-    blue)    printf "\033[34m%s\033[0m" "$text" ;;
-    bold)    printf "\033[1m%s\033[0m" "$text" ;;
-    cyan)    printf "\033[36m%s\033[0m" "$text" ;;
-    green)   printf "\033[32m%s\033[0m" "$text" ;;
-    magenta) printf "\033[35m%s\033[0m" "$text" ;;
-    red)     printf "\033[31m%s\033[0m" "$text" ;;
-    white)   printf "\033[38m%s\033[0m" "$text" ;;
-    yellow)  printf "\033[33m%s\033[0m" "$text" ;;
-    *)       printf "\033[30m%s\033[0m" "$text" ;;
-  esac
+	case $color in
+	blue) printf "\033[34m%s\033[0m" "$text" ;;
+	bold) printf "\033[1m%s\033[0m" "$text" ;;
+	cyan) printf "\033[36m%s\033[0m" "$text" ;;
+	green) printf "\033[32m%s\033[0m" "$text" ;;
+	magenta) printf "\033[35m%s\033[0m" "$text" ;;
+	red) printf "\033[31m%s\033[0m" "$text" ;;
+	white) printf "\033[38m%s\033[0m" "$text" ;;
+	yellow) printf "\033[33m%s\033[0m" "$text" ;;
+	*) printf "\033[30m%s\033[0m" "$text" ;;
+	esac
 }
 # helpers ---
