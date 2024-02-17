977d3e7fe0a9c27901420952fa1099e4bc4c6e1c
Christian Angermann <28717+cange@users.noreply.github.com>
2024-02-17 01:01:55 +0100

    feat(nvim): isolate linting from formatting

---
+++ nvim/lua/cange/plugins/linting.lua
@@ -0,0 +1,54 @@
+return {
+  { -- loads formatter and linter
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      ensure_installed = {
+        "eslint",
+        "jsonlint",
+        "markdownlint",
+        "rubocop",
+        "stylelint",
+        "yamllint",
+      },
+      auto_update = true,
+    },
+  },
+
+  { -- linting
+    "mfussenegger/nvim-lint",
+    lazy = true,
+    event = { "BufWritePost", "TextChanged" },
+    opts = {
+      json = { "jsonlint" },
+      markdown = { "markdownlint" },
+      ruby = { "rubocop" },
+      yaml = { "yamllint" },
+      -- css
+      css = { "stylelint" },
+      scss = { "stylelint" },
+      -- js
+      javascript = { "eslint" },
+      typescript = { "eslint" },
+      javascriptreact = { "eslint" },
+      typescriptreact = { "eslint" },
+      svelte = { "eslint" },
+      vue = { "eslint" },
+    },
+    keys = {
+      { "<leader>l", "<cmd>lua R('lint').try_lint()<CR>", desc = "Trigger linting for current file" },
+    },
+    config = function(_, opts)
+      local lint = require("lint")
+      lint.linters_by_ft = opts
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("lint", { clear = true }),
+        callback = function() lint.try_lint() end,
+      })
+    end,
+  },
+}
