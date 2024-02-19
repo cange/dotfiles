b1e20b24c71bbe34f1ef06cc6b4da951e05f8d28
Christian Angermann <28717+cange@users.noreply.github.com>
2024-02-19 20:01:16 +0100

    fix(nvim): remove duplicates icons with statusline

--- nvim/lua/lualine/util.lua
+++ nvim/lua/lualine/util.lua
@@ -9,4 +9,19 @@ function M.truncate(content, max_len)
   return #content > max_len and string.sub(content, 1, max_len - 1) .. "…" or content
 end
 
+---Ensures unique values within a string like table
+---@param list string[]
+---@return string[]
+function M.list_dedupe(list)
+  local result = {}
+  local seen = {}
+  for _, item in ipairs(list) do
+    if not seen[item] then
+      seen[item] = true
+      table.insert(result, item)
+    end
+  end
+  return result
+end
+
 return M
