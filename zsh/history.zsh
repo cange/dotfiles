546af53904dab1e34a5a6bb6e524824e7059b0d5
Christian Angermann <28717+cange@users.noreply.github.com>
2024-03-08 23:31:37 +0100

    fix(zsh): command history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,14 +2,13 @@
 # History Configuration
 # https://www.soberkoder.com/better-zsh-history/
 ##############################################################################
-HISTDUP=erase               # Erase duplicates in the history file
-HISTFILE=~/.zsh_history     # path/location of the history file
-HISTSIZE=100000             # number of commands that are loaded into memory
-SAVEHIST=100000             # number of commands that are stored
-
-setopt APPEND_HISTORY       # Append history to the history file (no overwriting)
-setopt HIST_FIND_NO_DUPS    # Do not display duplicates in the history list
-setopt INC_APPEND_HISTORY   # Immediately append to the history file, not just when a term is killed
-setopt SHARE_HISTORY        # Share history across terminals
-
-export HISTTIMEFORMAT="[%F %T] " #
+HISTDUP=erase             # Erase duplicates in the history file
+HISTFILE=~/.zsh_history   # path/location of the history file
+HISTSIZE=100000           # number of commands that are loaded into memory
+SAVEHIST=100000           # number of commands that are stored
+setopt APPEND_HISTORY     # Append history to the history file (no overwriting)
+setopt HIST_FIND_NO_DUPS  # Do not display duplicates in the history list
+setopt INC_APPEND_HISTORY # Immediately append to the history file, not just when a term is killed
+setopt SHARE_HISTORY      # Share history across terminals
+export HISTTIMEFORMAT="[%F %T] "
+export HISTFILE=~/.zsh_history
