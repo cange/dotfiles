4922843ade78cfd57e8d650aa909b2b48c376c91
Christian Angermann <28717+cange@users.noreply.github.com>
2024-03-09 17:13:31 +0100

    refactor(nvim): drop reload vs require

--- nvim/lua/cange/init.lua
+++ nvim/lua/cange/init.lua
@@ -3,9 +3,9 @@
 -- 1st : setup
 require("cange.globals")
 Cange = require("cange.utils")
-Cange.reload("cange.options")
+require("cange.options")
 -- 2st : plugins lazy loading
-Cange.reload("cange.lazy")
+require("cange.lazy")
 -- 3th : rest
-Cange.reload("cange.autocmds")
-Cange.reload("cange.keymaps")
+require("cange.autocmds")
+require("cange.keymaps")
