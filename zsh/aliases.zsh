acf447939d71ee6161238080babaa880751cf336
Christian Angermann <28717+cange@users.noreply.github.com>
2025-01-26 16:11:54 +0100

    feat(zsh): advanced git switch dialog

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -101,24 +101,24 @@ function git_main_branch() {
   echo master
 }
 
+# use fzf prompt to switch to a branch
+function git_switch_branch() {
+  local branch=$(git branch --all | fzf --prompt="Switch branch: " --height=~50% --layout=reverse --border --exit-0)
+  if [[ -n $branch ]]; then
+    git switch $(echo $branch | sed 's/.* //')
+  fi
+}
+
 # --- Aliases ---
 # (sorted alphabetically)
 
 alias g='git'
 # --- add
 alias ga='git add'
-alias gaa='git add --all'
-alias gapa='git add --patch'
 # --- branch
 alias gb='git branch'
 alias gba='git branch --all'
-alias gbd='git branch --delete'
-# --- bisect
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
+alias gbs='git_switch_branch'
 #
 alias gcl='git clone --recurse-submodules'
 alias gclean='git clean --interactive -d'
@@ -129,36 +129,30 @@ alias grm='git rm'
 alias gcb='git checkout -b'
 alias gcm='git checkout $(git_main_branch)'
 alias gco='git checkout'
+# --- switch
+alias gsw='git switch'
+alias gswm='git switch $(git_main_branch)'
 # --- commits
 alias gc='git commit --verbose'
 alias gcmsg='git commit --message'
-alias gcs='git commit --gpg-sign'
-alias gcss='git commit --gpg-sign --signoff'
-alias gcssm='git commit --gpg-sign --signoff --message'
 # --- fetching
 alias gf='git fetch'
 alias gl='git pull --prune'
-alias glr='git pull --rebase'
 # --- merge/rebase
 alias grb='git rebase'
 alias grbm='git rebase $(git_main_branch)'
 # --- logging
 alias glg="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
 alias glgg='git log --graph --decorate --all'
-alias glgm='glg --max-count=10'
 # ---- push
 alias gp='git push'
 alias gpfl='git push --force-with-lease' # safer than --force
 # --- status
-alias gss='git status --short'
 alias gst='git status'
 # --- stash
 alias gsta='git stash save'
 alias gstl='git stash list'
 alias gstp='git stash pop'
-# --- switch
-alias gsw='git switch'
-alias gswm='git switch $(git_main_branch)'
 # git ---
 #
 
